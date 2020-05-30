47889be30650a545e981abccc002851d2c930417
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-30 01:31:33 +0100

    Basic express with HBS setup created.

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'layout',
+    extname: 'hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.get('/', async (req, res) => {
+    let name = "Jacob"
+    res.render('index', {
+        name
+    });
+})
+
+app.listen(PORT || 3000, () => {
+    console.log(`server listening on ${PORT}`);
+
+})
